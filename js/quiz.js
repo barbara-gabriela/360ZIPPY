594e646383cc730ea53085968a3e7193146e4605
Teresa Marques <75506463+teresamarques@users.noreply.github.com>
2021-02-09 20:38:36 +0000

    changes

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+function generateQuiz(question, quizContainer, buttons){
+    function showQuestions(question, quizContainer){
+
+    }
+    function submitResults(question, quizContainer, buttons){
+
+    }
+    showQuestions(question, quizContainer);
+    submitResults.onclick = function(){
+        submitResults(question, quizContainer, buttons);
+    }
+}
+
+//perguntas
